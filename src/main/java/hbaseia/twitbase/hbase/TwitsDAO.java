97a2302e972e73f3ed64b4522d3956212baf255f
dymitr-zalunin <dymitr.zalunin@gmail.com>
2015-01-25 15:01:38 +0100

    Added TwitsDAO.Twit class fot internal usage

--- src/main/java/hbaseia/twitbase/hbase/TwitsDAO.java
+++ src/main/java/hbaseia/twitbase/hbase/TwitsDAO.java
@@ -1,8 +1,14 @@
 package hbaseia.twitbase.hbase;
 
+import org.apache.hadoop.hbase.CellUtil;
 import org.apache.hadoop.hbase.client.HConnection;
+import org.apache.hadoop.hbase.client.Result;
 import org.apache.hadoop.hbase.util.Bytes;
 import org.apache.log4j.Logger;
+import org.joda.time.DateTime;
+import utils.Md5Utils;
+
+import java.util.Arrays;
 
 public class TwitsDAO {
 
@@ -11,8 +17,10 @@ public class TwitsDAO {
 
     public static final byte[] USER_COL = Bytes.toBytes("user");
     public static final byte[] TWIT_COL = Bytes.toBytes("twit");
+    private static final int longLength = 8; // bytes
 
     private static final Logger log = Logger.getLogger(UsersDAO.class);
+
 
     private HConnection connection;
 
@@ -20,5 +28,26 @@ public class TwitsDAO {
         this.connection = connection;
     }
     
-    
+    private static class Twit extends hbaseia.twitbase.model.Twit {
+
+        private Twit(Result r) {
+            this(
+                    CellUtil.cloneValue(r.getColumnLatestCell(TWITS_FAM, USER_COL)),
+                    Arrays.copyOfRange(r.getRow(), Md5Utils.MD5_LENGTH, Md5Utils.MD5_LENGTH + longLength),
+                    CellUtil.cloneValue(r.getColumnLatestCell(TWITS_FAM, TWIT_COL)));
+        }
+
+        private Twit(byte[] user, byte[] dt, byte[] text) {
+            this(
+                    Bytes.toString(user),
+                    new DateTime(-1 * Bytes.toLong(dt)),
+                    Bytes.toString(text));
+        }
+
+        private Twit(String user, DateTime dt, String text) {
+            this.user = user;
+            this.dt = dt;
+            this.text = text;
+        }
+    }
 }
